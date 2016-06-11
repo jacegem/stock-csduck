cbff5aff23179387a7a5a701a877c269f09045a5
jacegem <jacegem@gmail.com>
2016-06-11 12:36:22 +0900

    테스트중.

--- stock-csduck/MainWindow.xaml.cs
+++ stock-csduck/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace stock_csduck
 {
     /// <summary>
@@ -20,9 +21,45 @@ namespace stock_csduck
     /// </summary>
     public partial class MainWindow : Window
     {
+        //*********************************************************
+        DSCBO1Lib.CpFConclusionClass m_CpFConclusion = new DSCBO1Lib.CpFConclusionClass();
+        DSCBO1Lib.FutureCurrClass m_FutureCurr = new DSCBO1Lib.FutureCurrClass();
+        DSCBO1Lib.FutureMst m_FutureMst = new DSCBO1Lib.FutureMst();
+
+        CPTRADELib.CpTdUtilClass m_TdUtil = new CPTRADELib.CpTdUtilClass();
+        CPTRADELib.CpTd6831Class m_6831 = new CPTRADELib.CpTd6831Class();
+        CPUTILLib.CpFutureCode m_FCode = new CPUTILLib.CpFutureCode();
+
+        Array m_arAccount;
+        //*********************************************************
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //주문 초기화
+            int nRet;
+            do
+            {
+                nRet = m_TdUtil.TradeInit(0);
+            } while (nRet != 0);
+
+            //주문이벤트등록
+            m_CpFConclusion.Received += new DSCBO1Lib._IDibEvents_ReceivedEventHandler(CpFConclusion_OnReceived);
+            m_CpFConclusion.Subscribe();
+        }
+
+        private void CpFConclusion_OnReceived()
+        {
+            Console.WriteLine("CpFConclusion_OnReceived");
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            //계좌번호 얻기                        
+            Console.WriteLine(m_TdUtil.AccountNumber.ToString());
+            m_arAccount = (Array)m_TdUtil.AccountNumber;
+            Console.WriteLine(m_arAccount.GetValue(0));
         }
     }
 }
